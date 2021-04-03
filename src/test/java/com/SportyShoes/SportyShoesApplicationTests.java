f528e059171aa5312f91de8cef76ddf1cd4988d1
PUSPITA <75912873+puspita-chatterjee@users.noreply.github.com>
2021-04-03 16:40:57 -0500

    Application Initial code commit !!

---
+++ src/test/java/com/SportyShoes/SportyShoesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.SportyShoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
